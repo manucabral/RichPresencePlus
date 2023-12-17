2d368dcde606e58d411f8a6d39d8ba7a2cd41483
Manu <mcabral@frba.utn.edu.ar>
2023-12-17 17:49:25 -0300

    feat: add metadata

--- rpp/__init__.py
+++ rpp/__init__.py
@@ -7,6 +7,11 @@ A simple Discord Rich Presence manager for desktop and web apps.
 from .logger import log
 from .presence import Presence
 
+__title__ = "Rich Presence Plus"
+__author__ = "Manuel Cabral"
+__version__ = "0.0.1"
+__license__ = "GPLv3"
+
 __all__ = [
     "log",
     "Presence",
