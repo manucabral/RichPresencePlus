b358b62c55a1bd45d33863d1344a27e18132954b
Manu <mandrescabral@gmail.com>
2024-10-13 19:25:26 -0300

    chore: update Youtube

--- presences/Youtube/main.py
+++ presences/Youtube/main.py
@@ -1,12 +1,20 @@
 import time
+import enum
 from rpp import extension, Presence, Runtime, Tab
+
+
+class Query(enum.Enum):
+    VIDEO_STREAM = 'Array.from(document.querySelectorAll(".video-stream")).find(element => element.duration)'
+    VIDEO_TITLE = 'document.querySelector("#title > h1 > yt-formatted-string")'
+    SHORT_AUTHOR = 'document.querySelectorAll("#channel-info #channel-name a")[document.querySelectorAll("#channel-info #channel-name a").length - 1]'
 
 
 @extension
 class Youtube(Presence):
 
     def __init__(self):
-        super().__init__(metadataFile=True)
+        super().__init__(metadata_file=True)
+        self.tab = None
         self.thumbnail = "https://i.ytimg.com/vi/{videoId}/hqdefault.jpg"
         self.logo = "https://cdn3.iconfinder.com/data/icons/social-network-30/512/social-06-1024.png"
 
@@ -14,51 +22,78 @@ class Youtube(Presence):
         return [
             tab
             for tab in tabs
-            if "www.youtube.com/watch" in tab.url or "www.youtube.com/shorts" in tab.url
+            if "www.youtube.com" in tab.url
+            or "www.youtube.com/watch" in tab.url
+            or "www.youtube.com/shorts" in tab.url
         ]
 
-    def extractVideoTitle(self, tab: Tab) -> str:
-        return (
-            tab.execute(
-                "document.querySelector('#title > h1 > yt-formatted-string').textContent"
-            )
-            or "Unknown"
-        )
+    def extractVideoTitle(self) -> str:
+        element = self.tab.execute(Query.VIDEO_TITLE.value)
+        if not element.objectId:
+            self.log.debug("Could not find title")
+            return "Unknown"
+        return self.tab.getProperties(element.objectId).textContent.value
 
-    def extractVideoId(self, tab: Tab) -> str:
-        if "v=" in tab.url:
-            videoId = tab.url.split("v=")[1]
+    def extractVideoId(self) -> str:
+        if "v=" in self.tab.url:
+            videoId = self.tab.url.split("v=")[1]
             return videoId.split("&")[0] if "&" in videoId else videoId
         return "Unknown"
 
-    def extractShortId(self, tab: Tab) -> str:
-        return tab.url.split("/shorts/")[1]
+    def extractShortId(self) -> str:
+        return self.tab.url.split("/shorts/")[1]
 
-    def extractVideoAuthor(self, tab: Tab) -> str:
-        return (
-            tab.execute('document.querySelector("#owner #text").textContent')
-            or "Unknown"
-        )
+    def extractVideoAuthor(self) -> str:
+        element = self.tab.execute('document.querySelector("#owner #text")')
+        if not element.objectId:
+            self.log.debug("Could not find author")
+            return "Unknown"
+        return self.tab.getProperties(element.objectId).textContent.value
 
-    def extractVideoAuthorUrl(self, tab: Tab) -> str:
-        return tab.execute('document.querySelector("#owner #text > a").href')
+    def extractVideoAuthorUrl(self) -> str:
+        element = self.tab.execute('document.querySelector("#owner #text > a")')
+        if not element.objectId:
+            self.log.debug("Could not find author url")
+            return "https://www.youtube.com"
+        props = self.tab.getProperties(element.objectId)
+        return props.href.value
+
+    def extractVideoDuration(self) -> int:
+        element = self.tab.execute(Query.VIDEO_STREAM.value)
+        if not element.objectId:
+            self.log.debug("Could not find duration")
+            return 0
+        return self.tab.getProperties(element.objectId).duration.value
+
+    def extractVideoCurrentTime(self) -> int:
+        element = self.tab.execute(Query.VIDEO_STREAM.value)
+        if not element.objectId:
+            self.log.debug("Could not find current time")
+            return 0
+        return self.tab.getProperties(element.objectId).currentTime.value
 
     def extractThumbnail(self, videoId: str) -> str:
         return self.thumbnail.format(videoId=videoId)
 
-    def extractVideoPlayback(self, tab: Tab) -> str:
-        return tab.execute("navigator.mediaSession.playbackState")
+    def extractVideoPlayback(self) -> str:
+        element = self.tab.execute("navigator.mediaSession")
+        if not element.objectId:
+            self.log.debug("Could not find playback state")
+            return "unknown"
+        props = self.tab.getProperties(element.objectId)
+        return props.playbackState.value
 
-    def extractShortAuthor(self, tab: Tab) -> str:
-        return tab.execute(
-            'document.querySelectorAll("#channel-info #channel-name a")[document.querySelectorAll("#channel-info #channel-name a").length - 1].textContent'
-        )
+    def extractShortAuthor(self) -> str:
+        element = self.tab.execute(Query.SHORT_AUTHOR.value)
+        if not element.objectId:
+            self.log.debug("Could not find author")
+            return "Unknown"
+        return self.tab.getProperties(element.objectId).textContent.value
 
-    def handleShort(self, tab: Tab):
-
-        shortId = self.extractShortId(tab)
+    def handleShort(self):
+        shortId = self.extractShortId()
         shortThumbnail = self.extractThumbnail(shortId)
-        shortAuthor = self.extractShortAuthor(tab)
+        shortAuthor = self.extractShortAuthor()
         shortAuthorUrl = "https://www.youtube.com/" + shortAuthor
 
         self.state = "Watching YouTube Short"
@@ -68,23 +103,35 @@ class Youtube(Presence):
         self.buttons = [
             {
                 "label": "Watch Short",
-                "url": tab.url,
+                "url": self.tab.url,
             },
             {
                 "label": "Author",
                 "url": shortAuthorUrl,
             },
         ]
-        self.log.debug(f"Updating short: {shortId}, url: {tab.url}")
+        self.log.info(f"Updating short: {shortId}, url: {self.tab.url}")
 
-    def handleVideo(self, tab: Tab):
+    def calculeTime(self, duration: int, currentTime: int) -> tuple[int, int]:
+        startTime = time.time() - int(currentTime)
+        endTime = startTime + int(duration)
+        return int(startTime), int(endTime)
 
-        videoId = self.extractVideoId(tab)
-        videoTitle = self.extractVideoTitle(tab)
-        videoAuthor = self.extractVideoAuthor(tab)
-        videoAuthorUrl = self.extractVideoAuthorUrl(tab)
+    def handleVideo(self):
+
+        videoId = self.extractVideoId()
+        videoTitle = self.extractVideoTitle()
+        videoAuthor = self.extractVideoAuthor()
+        videoAuthorUrl = self.extractVideoAuthorUrl()
         videoThumbnail = self.extractThumbnail(videoId)
-        videoPlayback = self.extractVideoPlayback(tab)
+        videoDuration = self.extractVideoDuration()
+        videoCurrentTime = self.extractVideoCurrentTime()
+        videoPlayback = self.extractVideoPlayback()
+
+        startTime, endTime = self.calculeTime(videoDuration, videoCurrentTime)
+
+        self.start = int(startTime)
+        self.end = int(endTime)
 
         self.small_text = "Playing" if videoPlayback == "playing" else "Paused"
         self.small_image = "play" if videoPlayback == "playing" else "pause"
@@ -96,14 +143,20 @@ class Youtube(Presence):
         self.buttons = [
             {
                 "label": "Watch on YouTube",
-                "url": tab.url,
+                "url": self.tab.url,
             },
             {
                 "label": "Author",
                 "url": videoAuthorUrl,
             },
         ]
-        self.log.debug(f"Updating video: {videoTitle}, url: {tab.url}")
+        self.log.info(f"Updating video: {videoTitle}, url: {self.tab.url}")
+
+    def handleTab(self, lastTab: Tab):
+        pass
+
+    def force_update(self):
+        pass
 
     def handleBrowsing(self):
         self.state = "Browsing..."
@@ -117,21 +170,40 @@ class Youtube(Presence):
         self.log.info("Started successfully")
 
     def on_update(self, runtime: Runtime):
+
         tabs = runtime.tabs()
-        tabs = self.extractYoutubeTabs(tabs)
+        tabs = self.extractYoutubeTabs(runtime.tabs())
+
         if not tabs:
             self.log.info("No YouTube tabs found")
             return
-        lastTab = tabs[0]
 
-        if "/shorts/" in lastTab.url:
-            self.handleShort(lastTab)
-        elif "/watch?" in lastTab.url:
-            self.handleVideo(lastTab)
+        lastTab = tabs[0]
+        if self.tab is None:
+            self.tab = lastTab
+            self.log.info("Setted initial tab")
+
+        elif self.tab.url != lastTab.url:
+            self.tab = lastTab
+            self.log.info("New tab detected")
+
+        else:
+            self.log.debug("Tab did not change, updating time")
+            videoDuration = self.extractVideoDuration()
+            videoCurrentTime = self.extractVideoCurrentTime()
+            startTime, endTime = self.calculeTime(videoDuration, videoCurrentTime)
+            self.start = int(startTime)
+            self.end = int(endTime)
+            return
+
+        self.tab.connect()
+
+        if "/shorts/" in self.tab.url:
+            self.handleShort()
+        elif "/watch?" in self.tab.url:
+            self.handleVideo()
         else:
             self.handleBrowsing()
-
-        time.sleep(1)
 
     def on_close(self):
         self.log.info("Closed")
