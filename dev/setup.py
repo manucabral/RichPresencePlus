17a81c170bf8686a8e635b05d3291b5368b7d8d8
Manu <mandrescabral@gmail.com>
2024-10-14 17:07:08 -0300

    chore: add setup.py for building RPPDev executable

---
+++ dev/setup.py
@@ -0,0 +1,23 @@
+from main import __title__, __version__, __description__
+from cx_Freeze import setup, Executable
+
+build_exe_options = {
+    "excludes": [
+        "tkinter",
+        "unittest",
+        "pypresence",
+        "rpp.Manager",
+        "rpp.Runtime",
+        "rpp.Presence",
+        "rpp.Browser",
+    ],
+    "packages": ["websocket", "presences"],
+}
+
+setup(
+    name=__title__,
+    version=__version__,
+    description=__description__,
+    options={"build_exe": build_exe_options},
+    executables=[Executable("main.py", base="Console", target_name="RPPDev")],
+)
