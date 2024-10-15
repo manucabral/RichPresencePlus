1113c43dc0b7416056119f9125f73e5f85ad9be6
Manu <mandrescabral@gmail.com>
2024-10-14 23:42:29 -0300

    refactor: update __all__ in __init__.py

--- rpp/__init__.py
+++ rpp/__init__.py
@@ -13,9 +13,10 @@ from .runtime import Runtime
 from .tab import Tab
 
 __title__ = "Rich Presence Plus"
+__description__ = "A simple Discord Rich Presence manager for custom desktop or web status."
 __version__ = "0.0.1"
 __author__ = "Manuel Cabral"
 __license__ = "GPL-3.0"
 
 
-__all__ = ["get_logger", "Manager", "Presence", "extension", "Runtime", "Tab"]
+__all__ = ["get_logger", "Manager", "Presence", "extension", "Browser", "Runtime", "Tab"]
