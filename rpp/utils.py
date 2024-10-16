c452717e96d96ee6068c32c9e98b111a0b8a6604
Manu <mandrescabral@gmail.com>
2024-10-15 23:55:27 -0300

    refactor:  add token authentication and improve code structure

--- rpp/utils.py
+++ rpp/utils.py
@@ -10,6 +10,24 @@ from .logger import get_logger
 log = get_logger("Utils")
 
 
+def generate_request(url: str, token: str = None) -> urllib.request.Request:
+    """
+    Generate a request object with the necessary headers.
+
+    Args:
+        url (str): URL to the request.
+        token (str): GitHub token for the request.
+
+    Returns:
+        urllib.request.Request: The request object.
+    """
+    headers = {}
+    log.info(token)
+    if token:
+        headers["Authorization"] = f"token {token}"
+    return urllib.request.Request(url, headers=headers)
+
+
 def download_github_file(url: str, file: str) -> None:
     """
     Download a file from GitHub.
@@ -21,7 +39,7 @@ def download_github_file(url: str, file: str) -> None:
     urllib.request.urlretrieve(url, file)
 
 
-def exist_github_folder(url: str) -> bool:
+def exist_github_folder(url: str, token: str = None) -> bool:
     """
     Check if a GitHub repository directory exists.
 
@@ -32,8 +50,7 @@ def exist_github_folder(url: str) -> bool:
         bool: Whether the directory exists.
     """
     try:
-        print(url)
-        response = urllib.request.urlopen(url, timeout=10)
+        response = urllib.request.urlopen(generate_request(url, token), timeout=10)
         data = json.loads(response.read())
         return bool(data)
     except urllib.error.HTTPError as exc:
@@ -41,7 +58,7 @@ def exist_github_folder(url: str) -> bool:
         return False
 
 
-def download_github_folder(url: str, folder: str) -> None:
+def download_github_folder(url: str, folder: str, token: str = None) -> None:
     """
     Download the contents of a GitHub repository directory.
 
@@ -54,7 +71,7 @@ def download_github_folder(url: str, folder: str) -> None:
     name = url.split("/")[-1]
     log.info(f"Downloading {name}...")
     try:
-        response = urllib.request.urlopen(url, timeout=10)
+        response = urllib.request.urlopen(generate_request(url, token), timeout=10)
         data = json.loads(response.read())
     except urllib.error.HTTPError as e:
         log.error(f"Failed to download {name}: {e}")
@@ -63,3 +80,44 @@ def download_github_folder(url: str, folder: str) -> None:
             download_github_file(
                 item["download_url"], os.path.join(folder, item["name"])
             )
+
+
+def get_available_presences(token: str = None) -> list[str]:
+    """
+    Get the available presences from the remote repository.
+
+    Args:
+        token (str): GitHub token for the request.
+
+    Returns:
+        list[str]: A list of available presences.
+    """
+    try:
+        response = urllib.request.urlopen(
+            generate_request(
+                "https://api.github.com/repos/manucabral/RichPresencePlus/contents/presences",
+                token,
+            ),
+            timeout=10,
+        )
+        data = json.loads(response.read())
+        return [item["name"] for item in data if item["type"] == "dir"]
+    except urllib.error.HTTPError as exc:
+        log.error(f"Failed to get available presences: {exc}")
+        return []
+
+
+def load_env() -> None:
+    """
+    Load the environment variables from the .env file.
+    """
+    try:
+        with open(".env", "r") as file:
+            for line in file:
+                key, value = line.strip().split("=")
+                os.environ[key] = value
+        log.info("Loaded .env file.")
+    except FileNotFoundError:
+        log.warning("No .env file found.")
+    except Exception as exc:
+        log.error(f"Failed to load .env file: {exc}")
