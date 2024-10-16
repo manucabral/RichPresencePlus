5a8ba891d4e5fbd28d015e6ed23966e2055b25dc
Manu <mcabral@frba.utn.edu.ar>
2024-10-15 23:48:49 -0300

    refactor(gui): update setup

--- gui/setup.py
+++ gui/setup.py
@@ -2,7 +2,7 @@ import rpp
 from cx_Freeze import setup, Executable
 
 # for now Windows only
-base = 'Win32GUI'
+base = "Win32GUI"
 
 build_exe_options = {
     "excludes": [
@@ -16,11 +16,13 @@ build_exe_options = {
 }
 
 executables = [
-    Executable('app.py', base=base, icon='logo.ico', target_name=rpp.__title__)
+    Executable("app.py", base=base, icon="logo.ico", target_name=rpp.__title__)
 ]
 
-setup(name=rpp.__title__,
-        version=rpp.__version__,
-        description=rpp.__description__,
-        options={"build_exe": build_exe_options},
-        executables=executables)
+setup(
+    name=rpp.__title__,
+    version=rpp.__version__,
+    description=rpp.__description__,
+    options={"build_exe": build_exe_options},
+    executables=executables,
+)
