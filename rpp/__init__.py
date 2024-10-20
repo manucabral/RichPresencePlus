71f08bcc6c3f256705a9039b7a5469eb630b8ba9
Manu <mandrescabral@gmail.com>
2024-10-20 00:09:55 -0300

    bump version to 0.0.5

--- rpp/__init__.py
+++ rpp/__init__.py
@@ -19,7 +19,7 @@ __title__ = "Rich Presence Plus"
 __description__ = (
     "A simple Discord Rich Presence manager for custom desktop or web status."
 )
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 __author__ = "Manuel Cabral"
 __license__ = "GPL-3.0"
 
