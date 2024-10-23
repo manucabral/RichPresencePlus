dfbf1f11442d502d288661811d992263b8930b13
Manu <mandrescabral@gmail.com>
2024-10-22 23:29:02 -0300

    refactor(rpp): bump to new version

--- rpp/__init__.py
+++ rpp/__init__.py
@@ -9,17 +9,18 @@ from .manager import Manager
 from .presence import Presence
 from .extension import extension
 from .rpc import ClientRPC, ActivityType
+from .steam import Steam, SteamAccount
 from .constants import Constants
 from .browser import Browser
 from .runtime import Runtime
-from .utils import load_env, get_available_presences
+from .utils import load_env, get_available_presences, get_steam_presence
 from .tab import Tab
 
 __title__ = "Rich Presence Plus"
 __description__ = (
     "A simple Discord Rich Presence manager for custom desktop or web status."
 )
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 __author__ = "Manuel Cabral"
 __license__ = "GPL-3.0"
 
@@ -36,5 +37,8 @@ __all__ = [
     "Runtime",
     "load_env",
     "get_available_presences",
+    "get_steam_presence",
     "Tab",
+    "Steam",
+    "SteamAccount",
 ]
