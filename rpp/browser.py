7b9412901b804d4468837c3f8fa1b050ce0ebedd
Manu <mandrescabral@gmail.com>
2024-10-24 21:10:47 -0300

    refactor(rpp.browser): add support for privilege-requiring paths (includes Arc browser compatibility)

--- rpp/browser.py
+++ rpp/browser.py
@@ -33,7 +33,7 @@ class Browser:
         with winreg.OpenKey(winreg.HKEY_CURRENT_USER, Constants.USER_CHOICE) as key:
             progid = winreg.QueryValueEx(key, "ProgId")[0]
             if not progid:
-                raise Exception("Can't find browser progid")
+                raise RuntimeError("ProgID not found. Internal registry error.")
             return progid
 
     def get_path(self) -> str:
@@ -43,13 +43,17 @@ class Browser:
         Returns:
             str: The browser path.
         """
+        custom_path = os.getenv("BROWSER_PATH")
+        if custom_path:
+            self.log.info("Using custom path: %s", custom_path)
+            return custom_path
         with winreg.OpenKey(
             winreg.HKEY_CLASSES_ROOT,
             Constants.BROWSER_PATH.format(progId=self.progid),
         ) as key:
             path = winreg.QueryValue(key, None).split('"')[1]
             if not os.path.exists(path):
-                self.log.warning(f"Browser path not found at {path}")
+                self.log.warning("Browser path not found at %s", path)
         return path
 
     def get_name(self) -> str:
@@ -70,19 +74,52 @@ class Browser:
             self.log.warning("Browser name not found. Using progid.")
             return self.progid
 
-    def kill(self) -> None:
+    def kill(self, admin: bool = False) -> None:
         """
-        Kill the browser process.
+        Kill the browser process using PowerShell with admin privileges if needed.
+
+        Args:
+            admin (bool, optional): Use admin privileges. Defaults to False.
         """
+        command = (
+            [
+                "powershell",
+                "-Command",
+                "Start-Process",
+                "powershell",
+                "-ArgumentList",
+                f"\"Stop-Process -Name '{self.process.replace(".exe", "")}' -Force\"",
+                "-Verb",
+                "RunAs",
+            ]
+            if admin
+            else Constants.KILL_BROWSER.format(process=self.process)
+        )
         try:
-            subprocess.run(
-                Constants.KILL_BROWSER.format(process=self.process),
+            # pylint: disable=consider-using-with
+            subprocess.Popen(
+                command,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
                 shell=True,
-                check=True,
+                text=True,
             )
-            self.log.info("Killed browser process.")
-        except subprocess.CalledProcessError:
-            self.log.error("Error killing browser process.")
+        except subprocess.CalledProcessError as exc:
+            if not self.running():
+                self.log.warning("Skipping kill, browser not running.")
+                return
+            self.log.warning("Cannot kill browser process. Trying as admin.")
+            self.log.error(exc)
+            if not admin:
+                self.kill(admin=True)
+        except subprocess.TimeoutExpired:
+            self.log.error("Timeout killing browser process.")
+        except PermissionError:
+            self.log.error("No permission to kill browser process.")
+        except FileNotFoundError:
+            self.log.error("Browser path not found.")
+        finally:
+            self.log.info("Browser killed.")
 
     def running(self) -> bool:
         """
@@ -98,13 +135,14 @@ class Browser:
                 stderr=subprocess.PIPE,
                 shell=True,
                 text=True,
+                check=True,
             )
             out = result.stdout.strip()
             return bool(out)
         except subprocess.CalledProcessError:
             return False
 
-    def start(self, remote_port: int = 9222) -> None:
+    def start(self, remote_port: int = 9222, admin: bool = False) -> None:
         """
         Start the browser with remote debugging.
 
@@ -114,15 +152,54 @@ class Browser:
         if self.running():
             self.log.warning("Refusing to start browser, already running.")
             return
-        try:
-            subprocess.Popen(
-                [
-                    self.path,
-                    f"--remote-debugging-port={remote_port}",
-                    f"--remote-allow-origins=http://127.0.0.1:{remote_port}",
-                    f"--remote-allow-origins=http://localhost:{remote_port}",
-                ]
+        command = (
+            [
+                "powershell",
+                "-Command",
+                "Start-Process",
+                f"'{self.path}'",
+                "-ArgumentList",
+                f"'--remote-debugging-port={remote_port}',"
+                f"'--remote-allow-origins=http://127.0.0.1:{remote_port}',"
+                f"'--remote-allow-origins=http://localhost:{remote_port}'",
+                "-Verb",
+                "RunAs",
+            ]
+            if admin
+            else [
+                self.path,
+                f"--remote-debugging-port={remote_port}",
+                f"--remote-allow-origins=http://127.0.0.1:{remote_port}",
+                f"--remote-allow-origins=http://localhost:{remote_port}",
+            ]
+        )
+
+        def as_admin():
+            self.log.warning(
+                "No permission to start browser normally. Trying to start as admin."
             )
-            self.log.info("Started.")
-        except Exception as exc:
-            self.log.error(f"Error starting browser: {exc}")
+            if not admin:
+                self.start(remote_port, True)
+
+        try:
+            subprocess.run(
+                command,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                shell=True,
+                text=True,
+                check=True,
+                timeout=10,
+            )
+        except PermissionError:
+            as_admin()
+        except FileNotFoundError:
+            self.log.error("Browser path not found.")
+        except subprocess.TimeoutExpired:
+            self.log.error("Timeout starting browser.")
+        except subprocess.CalledProcessError as exc:
+            self.log.error("Error starting browser.")
+            self.log.error(exc)
+            as_admin()
+        finally:
+            self.log.info("Browser started.")
